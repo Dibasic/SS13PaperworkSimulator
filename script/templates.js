75e8325f363b82ad46c497ae4fac4b5a8829a07a
Ivan Rubinson <soryy708@gmail.com>
2019-03-06 21:07:20 +0200

    Load templates dynamically (from json) (#14)
    Fixes #4

---
+++ script/templates.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $.getJSON('./templates/index.json', null, function(data) {
+        var html = constructHtml(data);
+        $('#template').html(html);
+        $('.controlgroup').controlgroup('refresh');
+    
+        function constructHtml(data) {
+            var str = '';
+            for (var i = 0; i < Object.keys(data).length; ++i) {
+                var key = Object.keys(data)[i];
+                var value = Object.values(data)[i];
+                if (jQuery.type(value) === 'string') {
+                    str += '<option value="' + value + '">' + key + '</option>';
+
+                } else if (jQuery.type(value) === 'object') {
+                    str += '<optgroup label="' + key + '">' + constructHtml(value) + '</optgroup>';
+
+                } else {
+                    console.warn('Unexpected value type in templates/index.json:', jQuery.type(value));
+                }
+            }
+            return str;
+        }
+    });
+});
