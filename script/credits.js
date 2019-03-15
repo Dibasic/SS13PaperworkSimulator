354f0b0d502401717ddf516a1a4c6723c0674a76
Dibasic <Dibasic@users.noreply.github.com>
2019-03-14 22:29:59 -0500

    Create credits page

---
+++ script/credits.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("#credits-dialog").dialog({
+        autoOpen: false
+    });
+
+    $('#credits-btn').click(function() {
+        $("#credits-dialog").dialog({
+        	minWidth: 465,
+        	minHeight: 660
+        });
+        $("#credits-dialog").dialog('open');
+    });
+});
