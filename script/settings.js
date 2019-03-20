f0f0bfd39a61fb04773d354123243ba74e8eef29
Ivan Rubinson <soryy708@gmail.com>
2019-03-20 19:41:14 +0200

    Revert fancy full screen transition

--- script/settings.js
+++ script/settings.js
@@ -17,11 +17,11 @@ $(function() {
         var target = $(e.target);
         var targetSelect = target.parent();
 
-        $('*').css('-webkit-transition', 'background-color 0.5s, color 0.5s');
-        $('*').css('-moz-transition', 'background-color 0.5s, color 0.5s');
-        $('*').css('-ms-transition', 'background-color 0.5s, color 0.5s');
-        $('*').css('-o-transition', 'background-color 0.5s, color 0.5s');
-        $('*').css('transition', 'background-color 0.5s, color 0.5s');
+        $('.controlgroup .control').css('-webkit-transition', 'none');
+        $('.controlgroup .control').css('-moz-transition', 'none');
+        $('.controlgroup .control').css('-ms-transition', 'none');
+        $('.controlgroup .control').css('-o-transition', 'none');
+        $('.controlgroup .control').css('transition', 'none');
 
         for (var i = 0; i < $('#theme-select option').length; ++i) {
             var option = $('#theme-select option')[i];
@@ -30,11 +30,11 @@ $(function() {
         $('body').addClass(targetSelect.val());
 
         setTimeout(function() {
-            $('*').css('-webkit-transition', '');
-            $('*').css('-moz-transition', '');
-            $('*').css('-ms-transition', '');
-            $('*').css('-o-transition', '');
-            $('*').css('transition', '');
+            $('.controlgroup .control').css('-webkit-transition', '');
+            $('.controlgroup .control').css('-moz-transition', '');
+            $('.controlgroup .control').css('-ms-transition', '');
+            $('.controlgroup .control').css('-o-transition', '');
+            $('.controlgroup .control').css('transition', '');
         }, 300);
     });
 });
